9937b98dcc5d274ea6996ee87040c16b092ecd76
CarloPg16 <l22212312@tectijuana.edu.mx>
2023-09-22 13:36:00 -0700

    Comentarios a las librerias

--- Smartcontracts/Partner/io/src/lib.rs
+++ Smartcontracts/Partner/io/src/lib.rs
@@ -1,20 +1,24 @@
 #![no_std]
 
+// Importación de las bibliotecas necesarias.
 use gmeta::{In, InOut, Metadata};
 use gstd::{msg, prelude::*, ActorId};
 use scale_info::TypeInfo;
 
+// Definición de un tipo de metadatos para el programa.
 pub struct ProgramMetadata;
-// HOLA, GAY EL QUE LO LEA
+
+// Implementación de la trait Metadata para ProgramMetadata.
 impl Metadata for ProgramMetadata {
-    type Init = In<InitPartner>;
-    type Handle = InOut<PartnerAction, PartnerEvent>;
-    type Reply = ();
-    type Others = ();
-    type Signal = ();
-    type State = Partner;
+    type Init = In<InitPartner>;     // Mensaje de inicialización del programa.
+    type Handle = InOut<PartnerAction, PartnerEvent>;     // Mensajes manejados por el programa con entrada y salida.
+    type Reply = (); // Mensaje de respuesta.
+    type Others = (); // Mensajes no especificados.
+    type Signal = (); // Utilizada para manejar señales o eventos en los smart contracts.
+    type State = Partner; // Tipo de estado en el programa, definido como partner.
 }
 
+// Definición del enum que representa los posibles estados de un socio o colaborador.
 #[derive(Encode, Decode, TypeInfo, PartialEq, Debug, Default)]
 pub enum PartnerState {
     #[default]
@@ -23,6 +27,7 @@ pub enum PartnerState {
     Closed,
 }
 
+// Definición de la estructura que representa a un socio o colaborador.
 #[derive(Encode, Decode, TypeInfo, Default)]
 pub struct Partner {
     pub user: ActorId,
@@ -31,6 +36,7 @@ pub struct Partner {
     pub state: PartnerState,
 }
 
+// Definición de la estructura que representa el mensaje de inicialización de un socio.
 #[derive(Encode, Decode, TypeInfo)]
 pub struct InitPartner {
     pub user: ActorId,
@@ -38,6 +44,7 @@ pub struct InitPartner {
     pub price: u128,
 }
 
+// Definición del enum que representa los eventos relacionados con un socio.
 #[derive(Encode, Decode, TypeInfo)]
 pub enum PartnerEvent {
     FondosDepositados,
@@ -45,32 +52,41 @@ pub enum PartnerEvent {
 }
 
 impl Partner {
+    // Implementación de una función para realizar un depósito.
     pub fn deposit(&mut self) {
+
+        // Verifica que el estado sea "EsperandoPago".
         assert_eq!(
             self.state,
             PartnerState::EsperandoPago,
             "State must be `AwaitingPayment`"
         );
 
+        // Verificación sobre el remitente del mensaje para saber si es un proyecto.
         assert_eq!(
             msg::source(),
             self.project,
             "The message sender must be a project"
         );
 
+        // Verificación sobre valor adjunto para que sea igual al precio establecido.
         assert_eq!(
             msg::value(),
             self.price,
             "The attached value must be equal to set price"
         );
 
+        // Actualiza el estado a "EsperandoEnvio" y envía una respuesta.
         self.state = PartnerState::EsperandoEnvio;
         msg::reply(PartnerEvent::FondosDepositados, 0)
             .expect("Error in reply PartnerEvent::FondosDepositados");
     }
+    
+    // Función de confirmación de la entrega.
     pub fn confirm_delivery(&mut self) {}
 }
 
+// Definición del enum que representa las posibles acciones de un socio.
 #[derive(Encode, Decode, TypeInfo)]
 pub enum PartnerAction {
     Depositar,
